59698ac7b41875a4c2b83cd8d10eb64185149da9
Raoul1996 <baobaozhou199606@gmail.com>
2019-01-12 16:53:19 +0800

    clone mobx template project
    Change-Id: I37c388657526bd0d553898c8dee27164c452ff35

---
+++ app/config/rootEpic.ts
@@ -0,0 +1,5 @@
+import { combineEpics } from "redux-observable";
+import { epicCheckLogin } from "../features/auth/epicAuth";
+
+// List all the epics here
+export default combineEpics(epicCheckLogin);
---
+++ app/src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { Store } from "../config/store";
+
+import Auth from "../features/auth";
+
+import "./styles/index.min.css";
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <Auth />
+  </Provider>,
+  document.getElementById("root")
+);
